2eedad17e60553c725b8cf554d3d964104fc87fa
Arun Acharya <arunacharya255@gmail.com>
2025-01-06 11:16:59 +0000

    feat: implement service to cache response from API

---
+++ app/Services/NewsService.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Guardian\GuardianRequestService;
+use App\Services\Interface\NewsServiceInterface;
+use Illuminate\Support\Facades\Cache;
+
+class NewsService
+{
+    /**
+     * Currently active news service
+     *
+     * @var NewsServiceInterface|string
+     */
+    public string $serviceClass;
+
+    /**
+     * Parameters for this service
+     *
+     * @var array<string>
+     */
+    public array $params;
+
+    /**
+     * Initialize this service
+     *
+     * @param  mixed  $serviceClass  The name of the class to get service from
+     * @return NewsService
+     */
+    public static function init($serviceClass = GuardianRequestService::class)
+    {
+        $service = new static;
+
+        $service->serviceClass = $serviceClass;
+
+        return $service;
+    }
+
+    /**
+     * Set parameters
+     *
+     * @param  array<string>  $params
+     * @return static
+     */
+    public function setParams($params)
+    {
+        $this->params = $params;
+
+        return $this;
+    }
+
+    /**
+     * Get key to store data in cache
+     */
+    public function cacheKey(): string
+    {
+        return $this->serviceClass.'&'.http_build_query($this->params);
+    }
+
+    /**
+     * Fetch data from currently active news service
+     *
+     * @return mixed
+     */
+    public function getData(): array
+    {
+        if (Cache::has($this->cacheKey())) {
+            return Cache::get($this->cacheKey());
+        }
+
+        $response = $this->serviceClass::getData($this->params);
+
+        Cache::put($this->cacheKey(), $response->json(), now()->addMinutes(config('panel.news_cache_invalidation_in_minutes')));
+
+        return $response->json();
+    }
+}
---
+++ config/panel.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'news_cache_invalidation_in_minutes' => env('NEWS_CACHE_INVALIDATION_IN_MINUTES', 10),
+];
